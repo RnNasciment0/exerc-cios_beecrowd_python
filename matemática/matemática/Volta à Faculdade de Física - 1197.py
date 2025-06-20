a12441f4b447b0757d5274c4300041a2ea159eb0
Renan <168815853+RnNasciment0@users.noreply.github.com>
2025-06-20 12:28:23 -0300

    Delete matemática/matemática directory

--- matemática/matemática/Volta à Faculdade de Física - 1197.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        velocidade, tempo = map(int, input().split())
-        distancia = velocidade * tempo * 2
-        print(f"{distancia}")
-    except EOFError:
-        break
